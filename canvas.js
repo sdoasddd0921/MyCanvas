3d1a91f4a4247d50b33d503742ef331e1cef9a1e
ZQ <850307035@qq.com>
2017-04-29 11:46:25 +0800

    before save

--- canvas.js
+++ canvas.js
@@ -4,6 +4,14 @@ var ctx    = canvas.getContext('2d');
 var radius = 10;
 var dragging = false;
 
+var minRad = 0.5,
+    maxRad = 100,
+    defaultRad = 20,
+    interval = 5,
+    radSpan = document.getElementById('radval'),
+    decRad = document.getElementById('decrad'),
+    incRad = document.getElementById('incrad');
+
 // document.body.clientWidth = window.innerWidth;
 // document.body.clientHeight = window.innerHeight;
 
@@ -49,14 +57,60 @@ var setRadius = function(newRadius){
     newRadius = maxRad;
   radius = newRadius;
   ctx.lineWidth = radius*2;
+
+  radSpan.innerHTML = radius;
 }
 
-var minRad = 0.5,
-    maxRad = 100,
-    defaultRad = 20,
-    radSpan = document.getElementById('radval'),
-    decRad = document.getElementById('decrad'),
-    incRad = document.getElementById('incrad');
+
+decRad.addEventListener('click', function(){
+  setRadius(radius-interval);
+});
+incRad.addEventListener('click', function(){
+  setRadius(radius==0.5?interval:radius+interval);
+});
+
+setRadius(defaultRad);
+
+/* colors */
+var colors = ['black', 'grey', 'white', 'red', 'orange',
+              'yellow','green', 'blue', 'indigo', 'violet'];
 
 
-/*2,11:41*/
+
+for(var i=0, n=colors.length;i<n;i++) {
+  var swatch = document.createElement('div');
+  swatch.className = 'swatch';
+  swatch.style.backgroundColor = colors[i];
+  swatch.addEventListener('click', setSwatch);
+  document.getElementById('colors').appendChild(swatch);
+}
+
+function setColor(color) {
+  ctx.fillStyle = color;
+  ctx.strokeStyle = color;
+  var active = document.getElementsByClassName('active')[0];
+  if(active) {
+    active.className = 'swatch';
+  }
+}
+
+function setSwatch(e) {
+  var swatch = e.target;
+  setColor(swatch.style.backgroundColor);
+  swatch.className += ' active';
+}
+
+setSwatch({target: document.getElementsByClassName('swatch')[0]});
+
+/* save */
+var saveButton = document.getElementById('save');
+
+saveButton.addEventListener('click', saveImage)
+
+function saveImage() {
+  var data = canvas.toDataURL();
+
+  
+
+  // window.open(data, '_blank', 'location=0, menubar=0');
+}
